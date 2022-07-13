6388c5b12b40dfec9c096fd87d610175816ef272
Thilina Ruwan <ict20838@fot.sjp.ac.lk>
2022-07-13 15:09:25 +0530

    Initial commit

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,11 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$qd5LlBMY$qohm7Wl8Tz24pSnOQGD4P0',
+    'role' => 'superadmin',
+    'email' => 'thilinaruwan112@gmail.com',
+  ),
+);
